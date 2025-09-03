eac8791fc0966f0a925b2564119f66fece08938b
tkara55 <164319459+tkara55@users.noreply.github.com>
2025-09-03 19:29:29 +0300

    Update food.gd

--- food.gd
+++ food.gd
@@ -2,19 +2,21 @@ extends AnimatedSprite2D
 var food_health = 60
 @onready var food: AnimatedSprite2D = $"."
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-func eat():
-	food_health -= 1
-	print(food_health)
-	if food_health % 10 == 0:
-		food.frame += 1
-		if food_health <= 0:
-			food.queue.free()
-		
+
+func eat(eater : Node):
+	if Global.game_running == 1:
+		food_health -= 1
+		print("%s yedi, kalan health: %d" % [eater.name, food_health])
+		if food_health % 10 == 0:
+			food.frame += 1
+			if food_health <= 0:
+				print("%s bitirdi!" % eater.name)
+				food.queue_free()
+				Global.game_running = 0
