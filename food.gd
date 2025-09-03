be3dca4d8b0b831b941781650a8bc71c05bd9674
tkara55 <164319459+tkara55@users.noreply.github.com>
2025-09-03 21:32:59 +0300

    Update food.gd

--- food.gd
+++ food.gd
@@ -1,10 +1,12 @@
 extends AnimatedSprite2D
 var food_health = 60
 @onready var food: AnimatedSprite2D = $"."
+var food_types = ["apple" , "pear"]
+var food_types_index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation = food_types[0]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,7 +18,27 @@ func eat(eater : Node):
 		print("%s yedi, kalan health: %d" % [eater.name, food_health])
 		if food_health % 10 == 0:
 			food.frame += 1
+			shake()
 			if food_health <= 0:
 				print("%s bitirdi!" % eater.name)
-				food.queue_free()
-				Global.game_running = 0
+				food_types_index += 1
+				
+				if food_types_index < food_types.size():
+					animation = food_types[food_types_index]
+					food_health = 60
+					food.frame = 0
+					
+				else :
+					food.queue_free()
+					Global.game_running = 0
+func shake():
+	var tween = create_tween()
+	var original_pos = position
+
+	for i in range(5): # 3 kere salla # İÇİ ÖNEMLİ
+		tween.tween_property(self, "position", original_pos + Vector2(4, 0), 0.05)
+		tween.tween_property(self, "position", original_pos - Vector2(4, 0), 0.05)
+		tween.tween_property(self, "position", original_pos + Vector2(0, 4), 0.05)
+		tween.tween_property(self, "position", original_pos - Vector2(0, 4), 0.05)
+
+	tween.tween_property(self, "position", original_pos, 0.05)
